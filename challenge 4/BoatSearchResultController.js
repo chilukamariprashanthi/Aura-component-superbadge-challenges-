0c19c2a4408119fc6888a39b5511ce057ece333c
prashanthi chilukamari <91251338+chilukamariprashanthi@users.noreply.github.com>
2021-09-27 16:19:47 +0530

    Create BoatSearchResultController.js

---
+++ challenge 4/BoatSearchResultController.js
@@ -0,0 +1,11 @@
+
+({
+    doInit : function(component, event, helper) {  
+    },
+   
+    doSearch : function(component, event, helper) {
+        var params = event.getParam('arguments');
+        component.set("v.boatTypeId", params.boatTypeId);
+                   helper.onSearch(component);
+    },
+})
